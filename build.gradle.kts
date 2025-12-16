58706b4209032d56c327138baa6ee18e8ee581cc
Tamboo <andrta@gmail.com>
2025-12-16 10:53:03 +0100

    chore: app module configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    // Android Application (for module :app)
+    alias(libs.plugins.androidApplication) apply false
+
+    // Android Library (for modules :core e :features)
+    alias(libs.plugins.androidLibrary) apply false
+
+    // Kotlin Android
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+
+    // Kotlin Serialization (for parsing JSON REST)
+    alias(libs.plugins.jetbrainsKotlinSerialization) apply false
+
+    // KSP (Kotlin Symbol Processing) - Essential for Room and Hilt
+    alias(libs.plugins.ksp) apply false
+
+    // Hilt (Dependency Injection)
+    alias(libs.plugins.hilt) apply false
+
+    // Apollo GraphQL (needed to generate models from the files .graphql)
+    alias(libs.plugins.apollo) apply false
 }
