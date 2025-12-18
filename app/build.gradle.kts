d705baebc3848eecbac8c94a4ec6414954f005f2
Tamboo <andrta@gmail.com>
2025-12-18 17:10:40 +0100

    chore: added new features:launches module to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,10 +44,6 @@ android {
         buildConfig = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
-    }
-
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -61,6 +57,7 @@ kotlin {
 
 dependencies {
     // --- Project modules ---
+    implementation(project(":features:launches"))
     implementation(project(":core:data"))
     implementation(project(":core:domain"))
     implementation(project(":core:localstorage"))
@@ -74,10 +71,10 @@ dependencies {
 
     // --- Compose (via BOM) ---
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
 
     // --- Navigation ---
     implementation(libs.androidx.navigation.compose)
@@ -87,6 +84,6 @@ dependencies {
     ksp(libs.hilt.compiler)
 
     // --- Debugging Tools ---
-    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.ui.test.junit4)
 }
---
+++ features/launches/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.example.launches"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        compose = true
+        viewBinding = true // this is needed for the XML views
+    }
+}
+
+dependencies {
+    implementation(project(":core:domain"))
+
+    // --- Hilt ---
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    // --- Android X / ViewModel ---
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose) // this is needed for collectAsStateWithLifecycle
+
+    // --- Jetpack Compose ---
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
+
+    // --- Coil ---
+    implementation(libs.coil.compose)
+    implementation(libs.coil.views)
+
+    // --- Testing ---
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
+}
